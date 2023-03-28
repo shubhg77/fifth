b38e035bed2d45488ca2afd9e7a04347c29d0e09
shubh sharma <109013021+shubhg77@users.noreply.github.com>
2023-03-28 13:49:52 +0530

    Half number pyramid

---
+++ Half Pyramid Pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main () {
+int tr,rowno=1;
+cin>>tr;
+	while(rowno<=tr){
+		int i=1;
+		while(i<=rowno){
+			cout<<i<<" ";
+			i=i+1;
+		}
+	
+	cout<<endl;
+	rowno=rowno+1;
+
+	}
+	return 0;
+}
+////////////////////////////////
+n=5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
